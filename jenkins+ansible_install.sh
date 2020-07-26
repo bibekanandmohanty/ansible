25c9142e600dd9edcd00559e9f84c36d68f54a6e
root <root@ip-172-31-46-162.ap-south-1.compute.internal>
2020-07-26 10:36:03 +0000

    Added jenkins installation steps

---
+++ jenkins+ansible_install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+username="test"
+pass="test"
+shift 2
+fname="$*"
+for each in vim wget httpd git java-1.8.0-openjdk-devel centos-release-scl rh-python36 scl-utils epel-release ; do yum install -y $each ; done
+yum update -y
+yum -y install ansible
+adduser "$username"
+echo "$username:$pass" | chpasswd
+usermod -aG wheel $username
+echo "$username    ALL=(ALL)       NOPASSWD:ALL" >> /etc/sudoers
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+service sshd restart
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+yum install jenkins -y
+systemctl start jenkins
+chkconfig jenkins on
+scl enable rh-python36 bash
