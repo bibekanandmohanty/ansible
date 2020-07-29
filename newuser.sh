318d1e71e764487c8b65c6dbe1d9eb54a5141962
bibek <bibek_786@rediffmail.com>
2020-07-29 18:15:24 +0530

    no commentswq

--- newuser.sh
+++ newuser.sh
@@ -14,6 +14,4 @@ yum update -y
 yum install tomcat -y
 echo "JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom -Djava.awt.headless=true -Xmx512m -XX:MaxPermSize=256m -XX:+UseConcMarkSweepGC"" >> /usr/share/tomcat/conf/tomcat.conf
 yum install tomcat-webapps tomcat-admin-webapps -y
-echo -e "<tomcat-users> \n<role rolename="admin"/> \n<role rolename="admin-gui"/> \n<role rolename="admin-script"/> \n<role rolename="manager"/> \n<role rolename="manager-gui"/> \n<role rolename="manager-script"/> \n<role rolename="manager-jmx"/> \n<role rolename="manager-status"/> \n<user name="bibek" password="bibek" roles="admin,manager,admin-gui,admin-script,manager-gui,manager-script,manager-jmx,manager-status" /> \n</tomcat-users>" >> /etc/tomcat/tomcat-users.xml
-systemctl restart tomcat
 
