25c3bed0306049183d674c08e64d11f937a2c1c8
bibek <bibek_786@rediffmail.com>
2020-07-29 19:10:22 +0530

    no commentswq

--- newuser.sh
+++ newuser.sh
@@ -11,6 +11,7 @@ echo "$username    ALL=(ALL)       NOPASSWD:ALL" >> /etc/sudoers
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 service sshd restart
 yum update -y
+for each in vim wget httpd git java-1.8.0-openjdk-devel centos-release-scl rh-python36 scl-utils epel-release ; do yum install -y $each ; done
 yum install tomcat -y
 echo "JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom -Djava.awt.headless=true -Xmx512m -XX:MaxPermSize=256m -XX:+UseConcMarkSweepGC"" >> /usr/share/tomcat/conf/tomcat.conf
 yum install tomcat-webapps tomcat-admin-webapps -y
