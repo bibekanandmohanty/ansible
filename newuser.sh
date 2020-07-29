0f3dd1da69355f244ef71f43eccaee06a0f697b5
bibek <bibek_786@rediffmail.com>
2020-07-29 16:21:42 +0530

    made changes to newuser.sh

--- newuser.sh
+++ newuser.sh
@@ -10,3 +10,10 @@ usermod -aG wheel $username
 echo "$username    ALL=(ALL)       NOPASSWD:ALL" >> /etc/sudoers
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 service sshd restart
+yum update -y
+yum install tomcat -y
+echo "JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom -Djava.awt.headless=true -Xmx512m -XX:MaxPermSize=256m -XX:+UseConcMarkSweepGC"" >> /usr/share/tomcat/conf/tomcat.conf
+yum install tomcat-webapps tomcat-admin-webapps -y
+echo -e "<tomcat-users> \n   <user username="admin" password="password" roles="manager-gui,admin-gui"/> \n</tomcat-users>" >> /usr/share/tomcat/conf/tomcat-users.xml
+systemctl restart tomcat
+
