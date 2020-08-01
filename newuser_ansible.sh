c1b575e20e3cb8e82ecb285f13a6c8b15c1a7a52
bibek <bibek_786@rediffmail.com>
2020-08-02 03:53:34 +0530

    added new_user file in ansible

---
+++ newuser_ansible.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+username="test"
+pass="test"
+shift 2
+fname="$*"
+adduser "$username"
+echo "$username:$pass" | chpasswd
+usermod -aG wheel $username
+echo "$username    ALL=(ALL)       NOPASSWD:ALL" >> /etc/sudoers
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+service sshd restart
+yum update -y
+for each in vim wget httpd git java-1.8.0-openjdk-devel centos-release-scl rh-python36 scl-utils epel-release ; do yum install -y $each ; done
+hostnamectl set-hostname client$1
