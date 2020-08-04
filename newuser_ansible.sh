773cb2cbe74f3f050845b76b593feed6434f4c46
Your Name <you@example.com>
2020-08-04 23:30:29 +0000

    no comments

--- newuser_ansible.sh
+++ newuser_ansible.sh
@@ -10,6 +10,4 @@ usermod -aG wheel $username
 echo "$username    ALL=(ALL)       NOPASSWD:ALL" >> /etc/sudoers
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 service sshd restart
-yum update -y
-for each in vim wget httpd git java-1.8.0-openjdk-devel centos-release-scl rh-python36 scl-utils epel-release ; do yum install -y $each ; done
 hostnamectl set-hostname client
